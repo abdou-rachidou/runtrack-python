b4e610bda262271cc3ef6f590c396df171d208e3
abdou-rachidou <abdou.rachidou-arouna@laplateforme.io>
2023-11-21 16:37:28 +0100

    La finalisation

--- jour02/Pour_aller_plus_loin/main.py
+++ jour02/Pour_aller_plus_loin/main.py
@@ -1,13 +1,29 @@
 def est_triangle(a, b, c):
-    return a + b > c and a + c > b and b + c > a
+    if a + b > c and b + c > a and a + c > b:
+        return True
+    else:
+        return False
 
-# Demander à l'utilisateur les longueurs des côtés du triangle
+def type_triangle(a, b, c):
+    if a == b == c:
+        return "Équilatéral"
+    elif a == b or b == c or a == c:
+        if a**2 + b**2 == c**2 or b**2 + c**2 == a**2 or a**2 + c**2 == b**2:
+            return "Rectangle et Isocèle"
+        else:
+            return "Isocèle"
+    else:
+        if a**2 + b**2 == c**2 or b**2 + c**2 == a**2 or a**2 + c**2 == b**2:
+            return "Rectangle"
+        else:
+            return "Quelconque"
+
 a = float(input("Entrez la longueur de la première côté (a) : "))
 b = float(input("Entrez la longueur de la deuxième côté (b) : "))
 c = float(input("Entrez la longueur de la troisième côté (c) : "))
 
-# Vérifier si les longueurs peuvent former un triangle
 if est_triangle(a, b, c):
-    print("Les longueurs peuvent former un triangle.")
+    type_tri = type_triangle(a, b, c)
+    print(f"Les longueurs {a}, {b} et {c} peuvent former un triangle de type {type_tri}.")
 else:
-    print("Les longueurs ne peuvent pas former un triangle.")
+    print(f"Les longueurs {a}, {b} et {c} ne peuvent pas former un triangle.")
