896fcf04bed9ead40bbf94ef41fb48a7f9833c83
abdou-rachidou <abdou.rachidou-arouna@laplateforme.io>
2023-11-22 12:57:05 +0100

    The first push of the day

---
+++ jour03/job02/main.py
@@ -0,0 +1,7 @@
+def My_print_name(name):
+    print(name)
+
+My_print_name("Rachide")
+My_print_name("Mathis")
+My_print_name("Abdel")
+My_print_name("Pierre")
---
+++ jour03/job03/main.py
@@ -0,0 +1,8 @@
+def Add(nombre1, nombre2):
+    sum = nombre1 + nombre2
+    print(f"{nombre1} + {nombre2} = {sum}")
+
+Add(5, 5)
+Add(10, 90)
+Add(25, 5)
+Add(12, 7)
