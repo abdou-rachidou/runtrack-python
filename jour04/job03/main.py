44f5edaba30062b541b2b1b8bc2f954dc7ee117d
abdou-rachidou <abdou.rachidou-arouna@laplateforme.io>
2023-11-23 11:13:00 +0100

    The third commit of the day

--- jour04/job03/main.py
+++ jour04/job03/main.py
@@ -1,9 +1,9 @@
 def fruits():
     my_fruit_list = ["pomme", "cerise", "orange"]
+    my_fruit_list.append("Melon")
     return my_fruit_list
 
 
 list = fruits()
-list.append("Melon")
 
 print(list)
---
+++ jour04/job04/main.py
@@ -0,0 +1,8 @@
+def fruits():
+    my_fruit_list = ["pomme", "cerise", "orange", "Melon"]
+    my_fruit_list[2] = "Mangue"
+    return my_fruit_list
+
+list = fruits()
+
+print(list)
